c470078570282558dc175ec6a1287b1a1c2d98c6
dengyongchi <dengyongchi@kuaishou.com>
2023-04-23 14:22:47 +0800

    feat: init

--- packages/components/src/Button/Button.vue
+++ packages/components/src/Button/Button.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+function handler() {
+  console.log('click');
+}
+</script>
 <template>
-  <button class="button">这是一个按钮组件</button>
+  <button class="button" @click="handler">这是一个按钮组件</button>
 </template>
-<script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.button {
+  width: 100px;
+  height: 100px;
+  background-color: aqua;
+}
+</style>
